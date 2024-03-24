afd60a5ef5a6bce22c41ffcc6f19340aafeb90ec
Loic Heinrich <heinrichloic@gmail.com>
2024-03-24 14:09:33 +0100

    Handling each part of an HTTP/1.1 request separately

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
 use std::{
-    fs, 
-    io::{prelude::*, BufReader}, 
-    net::{TcpListener, TcpStream},
-    thread,
-    time::Duration
+    collections::HashMap, fs, io::{prelude::*, BufReader}, net::{TcpListener, TcpStream}, thread, time::Duration
 };
 use rustbook_capstone_server::ThreadPool;
 
@@ -22,15 +18,42 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|res| res.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let mut buf_reader = BufReader::new(&mut stream);
+    let mut request_line = String::new();
+    buf_reader.read_line(&mut request_line).expect("Expected a request line");
+    request_line = request_line.trim().to_string();
+
+    let mut headers = String::new();
+    loop {
+        let read_result = buf_reader.read_line(&mut headers);
+        if let Ok(size) = read_result {
+            if size == 2 {
+                break;
+            }
+        }
+    }
+
+    let headers: HashMap<_, _> = headers.lines().map(|header| {
+        if header.len() == 0 {
+            return ("ENDHEADER".to_string(), "".to_string());
+        }
+        let colon = header.find(':').expect("Non empty header field should contain a colon \":\"");
+        let (name, value) = (header[..colon].to_string(), header[colon+1..].to_string());
+        return (name, value);
+    }).collect();
+
+    // TODO: Refactor body handling
+    if let Some(content_length) = headers.get("Content-Length") {
+        let length = content_length.parse::<usize>().expect("Expected Content-Length to be of type int");
+        let mut body = vec![0u8; length];
+        buf_reader.read_exact(&mut body).expect("Body should be the length of Content-Length");
+
+        println!("Body: {:#?}", body);
+    }   
 
 
-    let (status_line, filename) = match &http_request[0][..] {
+    // TODO: Refactor routing to be more generic
+    let (status_line, filename) = match &request_line[..] {
         "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
         "GET /sleep HTTP/1.1" => {
             thread::sleep(Duration::from_secs(5));
